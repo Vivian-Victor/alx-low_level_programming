3a25886549e75649b67b064a912cee551e80ca3e
Vivian-Victor <viviananyango2015@gmail.com>
2023-02-16 14:51:05 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-lude <stdio.h>
+#include <stdio.h>
 /**
- *  * main - main block
- *   * Return: o
- *    */
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {
-	        printf("\"with proper grammar, but the outcome is a piece of art,");
-	       	return (0);
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
 }
