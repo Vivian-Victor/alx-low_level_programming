c5fc43d4eeab79126bc4ad020986c6ce04b4408f
Vivian-Victor <viviananyango2015@gmail.com>
2023-02-17 00:36:37 +0300

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c programm that prints a new line to the standard error
+ * main - A c programm that prints a word followed by a new line to the standard error
  *  Return 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
