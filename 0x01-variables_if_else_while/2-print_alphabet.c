80e337d71198dd8870cdb4119b792bfc18bf8a84
Vivian-Victor <viviananyango2015@gmail.com>
2023-02-17 17:03:03 +0300

    alphabet in loercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+
+		}
+	putchar('\n');
+	return (0);
+}
