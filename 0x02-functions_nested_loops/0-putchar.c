1c8b60ac8956575c624c0e43dc3316513e8d7dbe
Vivian-Victor <viviananyango2015@gmail.com>
2023-02-21 21:40:11 +0300

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - A program that prints putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	int x = 0;
+
+	while (x < 7)
+	{
+		_putchar(c[x]);
+		x++;
+	}
+	_putchar('\n');
+	return (0);
+}
