916d44e831d9b214d810ff2eaaa7bfb04d4b8cef
Vivian-Victor <viviananyango2015@gmail.com>
2023-02-22 02:16:44 +0300

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - A function that prints 50 Fibonacci numbers
+ * Return:0
+ */
+int main(void)
+{
+	int x = 0;
+	long int a = 0, b = 1, next;
+
+	while (x < 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (x < 49)
+		{
+			printf(", ");
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
