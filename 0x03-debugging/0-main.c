f19066a47999fa6eb3c454c520c053a440f3e6e7
Vivian-Victor <viviananyango2015@gmail.com>
2023-02-23 09:35:46 +0300

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check whether positive or negetive integer
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
